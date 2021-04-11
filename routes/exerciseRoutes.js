b8c6f91d1f368d467d703d790d473fe3f8556e39
jarred <jarred.caris@gmail.com>
2021-04-11 15:43:28 +0930

    starting routes

---
+++ routes/exerciseRoutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const path = require('path')
+const exerciseRoutes = require("./exerciseRoutes.js");
+
+// get, put, post /api/workouts
+// get /api/workouts/range
+
+router.use('/', exerciseRoutes);
+
+router.get('/', async (req, res) => {
+  try {
+      res.sendFile(path.join(__dirname, '../public/index.html'))
+  } catch (err) {
+      res.status(500).json(err)
+  }
+})
+
+router.get('/exercises', async (req, res) => {
+  try {
+      res.sendFile(path.join(__dirname, '../public/exercise.html'))
+  } catch (err) {
+      res.status(500).json(err)
+  }
+})
+
+router.get('/stats', async (req, res) => {
+  try {
+      res.sendFile(path.join(__dirname, '../public/stats.html'))
+  } catch (err) {
+      res.status(500).json(err)
+  }
+})
+module.exports = router;
